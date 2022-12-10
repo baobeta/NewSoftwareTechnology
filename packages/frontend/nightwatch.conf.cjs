05cd96f63120e2e925a58b61f3179b644cadc252
baobeta <lequocbao29072001@gmail.com>
2022-12-10 16:30:40 +0700

    add nightwatch test

--- packages/frontend/nightwatch.conf.cjs
+++ packages/frontend/nightwatch.conf.cjs
@@ -1,21 +1,6 @@
 const chrome = require('chromedriver');
-//
-// Refer to the online docs for more details:
-// https://nightwatchjs.org/guide/configuration/nightwatch-configuration-file.html
-//
-//  _   _  _         _      _                     _          _
-// | \ | |(_)       | |    | |                   | |        | |
-// |  \| | _   __ _ | |__  | |_ __      __  __ _ | |_   ___ | |__
-// | . ` || | / _` || '_ \ | __|\ \ /\ / / / _` || __| / __|| '_ \
-// | |\  || || (_| || | | || |_  \ V  V / | (_| || |_ | (__ | | | |
-// \_| \_/|_| \__, ||_| |_| \__|  \_/\_/   \__,_| \__| \___||_| |_|
-//             __/ |
-//            |___/
-//
 
 module.exports = {
-  // An array of folders (excluding subfolders) where your tests are located;
-  // if this is not specified, the test source must be passed as the second argument to the test runner.
   src_folders: ['tests'],
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
--- packages/frontend/src/components/Modal/AddTopicAdmin.vue
+++ packages/frontend/src/components/Modal/AddTopicAdmin.vue
@@ -128,7 +128,7 @@
             <button
               v-else
               type="button"
-              class="text-white bg-red-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              class="button-add-topic-admin text-white bg-red-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
               disabled
             >
               Save
